0af7b60e69f5d8ede8f9e6d5df783782e31db0ca
Phuclinh-coder <baclinhdzpro@gmail.com>
2022-10-21 00:31:30 +0700

    2

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,25 +2,46 @@ var clbListtv = document.querySelectorAll('.clb-list-tv')
 var nextBtn = document.querySelector('.clb-next')
 var prevBtn = document.querySelector('.clb-prev')
 
+/*--    
+        Tabs
+    -----------------------------------*/  
+    const tabs = document.querySelectorAll('[data-tab-target]')
+    const tabContents = document.querySelectorAll('.tab-table-active')
 
-const BuyBtns = document.querySelectorAll('.js-register')
-            const modal = document.querySelector('.js-modal')
-            const close = document.querySelector('.js-modal-close')
-            const ModalContainer = document.querySelector('.js-modal-container')
-
-            function ShowBuyTickets(){
-                modal.classList.add('open')
-            }
-
-            function HideBuyTickets(){
-                modal.classList.remove('open')
-            }
-            for( const BuyBtn of BuyBtns){
-                BuyBtn.addEventListener('click',ShowBuyTickets)
-            }
-
-            close.addEventListener('click',HideBuyTickets)
-            modal.addEventListener('click',HideBuyTickets)
-            ModalContainer.addEventListener('click', function(event){
-                event.stopPropagation()
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.tabTarget)
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active')
             })
+            tabs.forEach(tab => {
+                tab.classList.remove('active')
+            })
+            tab.classList.add('active')
+            target.classList.add('active')
+        })
+    })
+
+
+// Modal js
+const regBtns = document.querySelectorAll('.js-register') 
+const modal = document.querySelector('.js-modal')
+const close = document.querySelector('.js-modal-close')
+const ModalContainer = document.querySelector('.js-modal-container')
+
+    function ShowRegister(){
+        modal.classList.add('open')
+    }
+
+    function HideRegister(){
+        modal.classList.remove('open')
+    }
+    for( const Btn of regBtns){
+        Btn.addEventListener('click',ShowRegister)
+    }
+
+    close.addEventListener('click',HideRegister)
+    modal.addEventListener('click',HideRegister)
+    ModalContainer.addEventListener('click', function(event){
+        event.stopPropagation()
+    })
